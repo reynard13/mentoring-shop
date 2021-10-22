4e5b407f6aed00289735739a9068b9ed58cd952e
Skoryi Serhii <well.another.attempt@gmail.com>
2021-10-23 00:56:23 +0300

    Develop feature to store user with HTTP request POST /users

---
+++ src/main/java/com/example/servingwebcontent/model/User.java
@@ -0,0 +1,13 @@
+package com.example.servingwebcontent.model;
+
+public class User {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
