9084fc48547ffa784b4a03e462b5dee573a5608e
vovan42002 <volodimirbogdan4@gmail.com>
2021-11-14 18:55:50 +0200

    Create CategoryRepository

--- src/main/java/org/example/mentoringshop/controllers/UserController.java
+++ src/main/java/org/example/mentoringshop/controllers/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("users")
+@RequestMapping("/users")
 public class UserController {
     @Autowired
     private UserService userService;
